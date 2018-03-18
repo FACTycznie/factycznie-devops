e3feea070421309c4997546195909c494e45bb17
Michał Cybulski <michal@cybulski.me>
2018-03-18 10:02:02 +0100

    Fixed nginx configuration in docker-compose.yml file.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 docker stack deploy -c docker-compose.yml --prune factycznie-app
 docker service update --force factycznie-app_frontend
 docker service update --force factycznie-app_backend
